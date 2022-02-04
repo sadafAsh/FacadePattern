b96d9602a34970eb23e5d4620ce2f1975223b5cc
test@gmail.com <test@gmail.com>
2022-02-04 17:53:46 +0000

    main complete

--- src/main/java/com/soj/designpattern/facade/Main.java
+++ src/main/java/com/soj/designpattern/facade/Main.java
@@ -3,11 +3,38 @@ package com.soj.designpattern.facade;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Scanner;
+
 public class Main {
-    static final Logger logger= LogManager.getLogger(Main.class.getName());
-    public static void main(String[] args){
-logger.debug("hello");
-        logger.debug("hello");
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+        int ch;
+        do {
+            logger.info("1.Iphone");
+            logger.info("2.Nokia");
+            logger.info("3.Oppo");
+            logger.info("4.Exit");
+            logger.info("enter a choice");
+            Scanner scanner = new Scanner(System.in);
+            ch = scanner.nextInt();
+            ShopKeeper shopKeeper = new ShopKeeper();
+            switch (ch) {
+                case 1:
+                    shopKeeper.iphoneSale();
+                    break;
+                case 2:
+                    shopKeeper.nokiaSale();
+                    break;
+                case 3:
+                    shopKeeper.oppoSale();
+                    break;
+                default:
+                    logger.debug("Nothing is purchased");
+                    break;
+            }
+        } while (ch != 4);
 
     }
 }
---
+++ src/main/java/com/soj/designpattern/facade/MobileShop.java
@@ -0,0 +1,6 @@
+package com.soj.designpattern.facade;
+
+public interface MobileShop {
+    public void modelNo();
+    public void price();
+}
